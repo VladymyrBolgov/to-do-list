52a721333c56a37a8b67aea05ce3e15f8c19c2c7
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-20 20:43:57 +0300

    п

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { tasksReducer } from './tasksSlice';
 import { filtersReducer } from './filtersSlice';
+
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
